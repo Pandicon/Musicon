dadb2431c190d69231f4be07247b8a4cfe90f4a0
Pandicon <70060103+Pandicon@users.noreply.github.com>
2021-05-23 22:55:05 +0200

    Adding error embeds

--- events/distube/error.js
+++ events/distube/error.js
@@ -1,12 +1,26 @@
 const Discord = require('discord.js');
 const skip = require("@commands/music/skip.js")
 const {
-    playSong: playSongColor
+    error: errorColor
 } = require("@conf/colors.json")
 
 module.exports = distube => {
     distube.on("error", (message, e) => {
         console.error(e)
-        message.channel.send("An error encountered: " + e);
+        message.channel.send(
+            new Discord.MessageEmbed()
+                .setAuthor(
+                    message.member.nickname || message.author.username,
+                    message.author.displayAvatarURL({
+                        dynamic: true
+                    })
+                )
+                .setColor(errorColor)
+                .setTitle("An error encountered")
+                .setDescription(e)
+                .setFooter(message.guild.me.user.tag, message.guild.me.user.displayAvatarURL({
+                    dynamic: true
+                }))
+        );
     });
 }
