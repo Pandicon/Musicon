e77ac8a1e30d6e9c812a1d83971e9b0b2c2d9150
Pandicon <70060103+Pandicon@users.noreply.github.com>
2021-05-24 23:57:27 +0200

    Changing plain text messages into embeds

--- events/distube/addSong.js
+++ events/distube/addSong.js
@@ -5,7 +5,30 @@ const {
 } = require("@conf/colors.json")
 
 module.exports = distube => {
-    distube.on("addSong", (message, queue, song) => message.channel.send(
+    distube.on("addSong", async(message, queue, song) => {
+        const embed = new Discord.MessageEmbed()
+        .setColor(playSongColor)
+        .setAuthor(
+            (await message.guild.members.fetch(song.user.id)).nickname || song.user.username,
+            song.user.displayAvatarURL({
+                dynamic: true
+            })
+        )
+        .setThumbnail(song.thumbnail.url)
+        .setDescription(`Added [${song.name}](${song.url}) to the queue.`)
+        .addFields({
+            name: "Requested by",
+            value: `${song.user}`,
+            inline: true
+        }, {
+            name: "Duration",
+            value: `${song.formattedDuration}`,
+            inline: true
+        })
+        .setFooter(`Published by ${song.info.videoDetails.ownerChannelName}, currently has ${parseInt(song.info.videoDetails.viewCount).toLocaleString()} view${song.info.videoDetails.viewCount == 1 ? "" : "s"}`)
+        message.channel.send(embed);
+    })
+    /*message.channel.send(
         `Added ${song.name} - \`${song.formattedDuration}\` to the queue by \`${song.user.tag}\``
-    ))
+    )*/
 }
