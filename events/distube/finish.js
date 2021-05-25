ccbe1fd134106cf2f6537349c503eea9481da230
Pandicon <70060103+Pandicon@users.noreply.github.com>
2021-05-25 22:59:44 +0200

    Update finish.js

--- events/distube/finish.js
+++ events/distube/finish.js
@@ -1,11 +1,20 @@
 const Discord = require('discord.js');
 const skip = require("@commands/music/skip.js")
 const {
-    playSong: playSongColor
+    warning
 } = require("@conf/colors.json")
 
 module.exports = distube => {
-    distube.on("finish", message => message.channel.send(
-        "No more songs in queue"
-    ))
+    distube.on("finish", async message => {
+        const embed = new Discord.MessageEmbed()
+        .setColor(warning)
+        .setAuthor(
+            message.guild.me.user.tag,
+            message.guild.me.user.displayAvatarURL({
+                dynamic: true
+            })
+        )
+        .setDescription("No more songs in queue.")
+        message.channel.send(embed);
+    })
 }
