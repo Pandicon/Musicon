b3385821a935ecf102f43268f0c45398c4d5b972
Pandicon <70060103+Pandicon@users.noreply.github.com>
2021-05-29 11:31:42 +0200

    Changing more plain text responses into embeds

--- events/distube/noRelated.js
+++ events/distube/noRelated.js
@@ -1,11 +1,20 @@
 const Discord = require('discord.js');
 const skip = require("@commands/music/skip.js")
 const {
-    playSong: playSongColor
+    warning
 } = require("@conf/colors.json")
 
 module.exports = distube => {
-    distube.on("noRelated", message => message.channel.send(
-        "Can't find related video to play. Stop playing music."
-    ))
+    distube.on("noRelated", message => {
+        const embed = new Discord.MessageEmbed()
+        .setColor(warning)
+        .setAuthor(
+            message.guild.me.user.tag,
+            message.guild.me.user.displayAvatarURL({
+                dynamic: true
+            })
+        )
+        .setDescription("Can't find related videos to play. Stop playing music.")
+        message.channel.send(embed);
+    })
 }
