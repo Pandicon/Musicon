e83d2ab58efc553c66f94aced2e304ce071de2c4
Pandicon <mkpanda235@gmail.com>
2021-05-30 11:29:02 +0200

    Renaming function

--- events/load-events.js
+++ events/load-events.js
@@ -5,12 +5,12 @@ const commandBase = require('@commands/command-base');
 module.exports = (client, distube) => {
     const commands = []
   
-    const readCommands = (dir) => {
+    const readEvents = (dir) => {
       const files = fs.readdirSync(path.join(__dirname, dir))
       for (const file of files) {
         const stat = fs.lstatSync(path.join(__dirname, dir, file))
         if (stat.isDirectory()) {
-          readCommands(path.join(dir, file))
+          readEvents(path.join(dir, file))
         } else if (file !== 'load-events.js') {
           const option = require(path.join(__dirname, dir, file))
           if(dir == "distube" && distube) {
@@ -22,7 +22,7 @@ module.exports = (client, distube) => {
       }
     }
     
-    readCommands('.')
+    readEvents('.')
 
     return commands
 }
